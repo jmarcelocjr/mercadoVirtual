b84cb79e28820f051d976eb0b6439df30d41a0d1
Giovani <giovaniamaro@hotmail.com>
2014-10-07 20:02:31 -0300

    carrinho de compras

--- controller/produto.controller.class.php
+++ controller/produto.controller.class.php
@@ -1,7 +1,7 @@
 <?php
 
 //Inclui a classe genérica CRUD
-require_once ("../functions/crud.class.php");
+require_once (__DIR__ . '/../functions/crud.class.php');
 
 class ProdutoController extends Crud {
 
@@ -16,7 +16,7 @@ class ProdutoController extends Crud {
 	//Listagem de todas as produto
 
 	public function lista($where = null) {
-		$sql = "SELECT produto.descricao as \'produto\', concat(quantidade.peso, \'-\', quantidade.unidade), setor.descricao as \'setor\', marca.descricao as \'marca\' FROM produto INNER JOIN quantidade on quantidade.id = produto.Quantidade_id INNER JOIN setor on setor.id = produto.Setor_id INNER JOIN produto_has_marca phm on produto.id = phm.Produto_id INNER JOIN marca on marca.id = phm.Marca_id";
+		$sql = "SELECT produto.id as 'codigo', produto.descricao as 'produto', concat(quantidade.peso, '-', quantidade.unidade) as 'quantidade', setor.descricao as 'setor', marca.descricao as 'marca' FROM produto INNER JOIN quantidade on quantidade.id = produto.Quantidade_id INNER JOIN setor on setor.id = produto.Setor_id INNER JOIN produto_has_marca phm on produto.id = phm.Produto_id INNER JOIN marca on marca.id = phm.Marca_id";
 		if ($where != null) {$sql .= " WHERE $where;";}
 		return $this->execute_query($sql);
 	}
