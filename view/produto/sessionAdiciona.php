076eb2fa64580495059eaba8fa34c0d8b7a0bbbf
Marcelo <jmarcelo.cjr@gmail.com>
2014-10-22 22:09:00 -0200

    atualização menu e comparação

--- view/produto/sessionAdiciona.php
+++ view/produto/sessionAdiciona.php
@@ -2,30 +2,29 @@
 
 session_start();
 
-$idProduto = 0;
 if (isset($_POST['idProduto'])) {
 	$idProduto = $_POST['idProduto'];
-}
 
-if (!isset($_SESSION['idProdutos'])) {
-	$_SESSION['idProdutos'] = array();
-}
-
-$isRepitido = false;
-if ($idProduto != 0) {
-	foreach ($_SESSION['idProdutos'] as $produto) {
-		if ($produto == $idProduto) {
-			$isRepitido = true;
-		}
+	if (!isset($_SESSION['idProdutos'])) {
+		$_SESSION['idProdutos'] = array();
 	}
-	if (!($isRepitido)) {
-		array_push($_SESSION['idProdutos'], $idProduto);
-		echo "true";
+
+	$isRepitido = false;
+	if ($idProduto != 0) {
+		foreach ($_SESSION['idProdutos'] as $produto) {
+			if ($produto == $idProduto) {
+				$isRepitido = true;
+			}
+		}
+		if (!($isRepitido)) {
+			array_push($_SESSION['idProdutos'], $idProduto);
+			echo "true";
+		} else {
+			echo "false";
+		}
 	} else {
 		echo "false";
 	}
-} else {
-	echo "false";
 }
 
 ?>
